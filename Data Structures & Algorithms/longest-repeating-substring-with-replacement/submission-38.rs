5fa19b81384cedca63a04abb053f063c71f49b17
José Afonso Cunha Conceição <jaconceicao07@gmail.com>
2026-09-08 20:01:24 +0100

    Add: longest-repeating-substring-with-replacement - submission-38

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-38.rs
@@ -0,0 +1,31 @@
+impl Solution {
+    pub fn character_replacement(s: String, k: i32) -> i32 {
+        let b = s.as_bytes();
+        let k = k as usize;
+
+        if k + 1 >= b.len() {
+            return b.len() as i32;
+        }
+
+        // Tabela compacta de apenas 104 bytes na Stack
+        let mut count = [0u32; 26];
+        let mut max_c = 0u32;
+        let mut left = 0usize;
+
+        for (right, &byte) in b.iter().enumerate() {
+            let idx = (byte - b'A') as usize;
+            count[idx] += 1;
+            
+            if count[idx] > max_c {
+                max_c = count[idx];
+            }
+
+            if right + 1 > left + (max_c as usize) + k {
+                count[(b[left] - b'A') as usize] -= 1;
+                left += 1;
+            }
+        }
+
+        (b.len() - left) as i32
+    }
+}
